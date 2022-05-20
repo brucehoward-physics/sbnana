a1a57905a56955d2c6d244a799d52489dbe774f6
Edward Tyley <e.tyley@sheffield.ac.uk>
2022-05-20 11:41:41 -0500

    Tidy indentation again

--- sbnana/CAFAna/XSec/Flux.h
+++ sbnana/CAFAna/XSec/Flux.h
@@ -7,39 +7,38 @@
 
 #include "sbnanaobj/StandardRecord/Proxy/FwdDeclare.h"
 
-namespace ana
-{
-  bool IsCCQEOnArgon(const caf::SRTrueInteractionProxy* nu, int pdg);
+namespace ana {
+bool IsCCQEOnArgon(const caf::SRTrueInteractionProxy* nu, int pdg);
 
-  bool IsNCQEOnArgon(const caf::SRTrueInteractionProxy* nu, int pdg);
+bool IsNCQEOnArgon(const caf::SRTrueInteractionProxy* nu, int pdg);
 
-  class FluxTimesNuclei: public Spectrum
-  {
+class FluxTimesNuclei : public Spectrum {
   public:
-    /// pdg PDG code for neutrino, -14,-12,+12,14
-    FluxTimesNuclei(INuTruthSource& src, const Binning& bins,
-                    const NuTruthCut& fidvol, int pdg, const NuTruthWeight& wgt = kNuTruthUnweighted);
+  /// pdg PDG code for neutrino, -14,-12,+12,14
+  FluxTimesNuclei(INuTruthSource& src, const Binning& bins,
+      const NuTruthCut& fidvol, int pdg, const NuTruthWeight& wgt = kNuTruthUnweighted);
 
-    TH1D* ToTH1(double pot,
-                Color_t col = kBlack,
-                Style_t style = kSolid,
-                EBinType bintype = kBinContent);
+  TH1D* ToTH1(double pot,
+      Color_t col = kBlack,
+      Style_t style = kSolid,
+      EBinType bintype = kBinContent);
+
   protected:
-    int fPdg;
-  };
+  int fPdg;
+};
 
-  class EnsembleFluxTimesNuclei: public EnsembleSpectrum
-  {
+class EnsembleFluxTimesNuclei : public EnsembleSpectrum {
   public:
-    /// pdg PDG code for neutrino, -14,-12,+12,14
-    EnsembleFluxTimesNuclei(INuTruthEnsembleSource& src, const Binning& bins,
-                    const NuTruthCut& fidvol, int pdg, const NuTruthWeight& wgt = kNuTruthUnweighted);
+  /// pdg PDG code for neutrino, -14,-12,+12,14
+  EnsembleFluxTimesNuclei(INuTruthEnsembleSource& src, const Binning& bins,
+      const NuTruthCut& fidvol, int pdg, const NuTruthWeight& wgt = kNuTruthUnweighted);
 
-    TH1D* ToTH1(double pot,
-                Color_t col = kBlack,
-                Style_t style = kSolid,
-                EBinType bintype = kBinContent);
+  TH1D* ToTH1(double pot,
+      Color_t col = kBlack,
+      Style_t style = kSolid,
+      EBinType bintype = kBinContent);
+
   protected:
-    int fPdg;
-  };
+  int fPdg;
+};
 }
